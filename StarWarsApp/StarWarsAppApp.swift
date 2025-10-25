86463bc67f80e48447ba351822e6a4df114094c8
masapple <masapple11@gmail.com>
2025-10-25 10:50:48 +0900

    Initial Commit

---
+++ StarWarsApp/StarWarsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsAppApp.swift
+//  StarWarsApp
+//
+//  Created by Masataka Miyagawa on 2025/10/25.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
